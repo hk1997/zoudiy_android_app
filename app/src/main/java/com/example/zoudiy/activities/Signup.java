83b75e879098beb95580aba073888cc55a509457
AD-Programmer <adboy9696@gmail.com>
2020-02-19 22:56:04 +0530

    Commit

--- app/src/main/java/com/example/zoudiy/activities/Signup.java
+++ app/src/main/java/com/example/zoudiy/activities/Signup.java
@@ -1,6 +1,7 @@
 package com.example.zoudiy.activities;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,7 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 }
