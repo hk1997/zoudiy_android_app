2656067bed6916af4ea11b330db457af98f563af
tutushubham <tutushubham@gmail.com>
2020-03-13 17:48:58 +0530

    3 part profile signup UIs made for each

---
+++ app/src/main/java/com/example/zoudiy/activities/AddNewAddr.java
@@ -0,0 +1,17 @@
+package com.example.zoudiy.activities;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.zoudiy.R;
+
+public class AddNewAddr extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_addr);
+    }
+
+}
